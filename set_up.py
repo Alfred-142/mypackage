530e6cc37309eafc756997c5148e8ffad3f4fb8b
Alfred.142 <wawerualfred14@gmail.com>
2026-04-03 06:32:03 +0300

    modified setup

--- set_up.py
+++ set_up.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
-    name = "mypackage",
-    version = "0.1",
-    packages = find_packages(exclude=["tests"]),
-    license = "MIT",
-    description = "EDSA example package",
-    long_description = open("Readme.MD"),
-    install_requires = ["Numpy"],
-    url = "Https//:github.com/alred_142/mypackage",
-    author ="alfred",
-    author_email = "wawerualfred14@gmail.com"
+    name="mypackage",
+    version="0.1",
+    packages=find_packages(exclude=["tests"]),
+    license="MIT",
+    description="EDSA example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=["numpy"],
+    url="https://github.com/alfred_142/mypackage",
+    author="alfred",
+    author_email="wawerualfred14@gmail.com",
+    python_requires=">=3.7",
 )
